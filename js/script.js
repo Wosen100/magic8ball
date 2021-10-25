7e5e9df95187a51abd6b657ce36f8ce70ad7b8a9
Wosen100 <83084085+Wosen100@users.noreply.github.com>
2021-10-25 19:39:11 -0400

    the project ended

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 const askbutton= document.getElementById("askbutton");
 const askInput= document.getElementById("askInput");
-const answers = ["yes" ,"No", "MayBe", "Ask Later"]
-const answerdiv = document.getElementById("answer")
-const asklaterImg = "https://wisernow.com/wp-content/uploads/2020/08/magic8ball.png"
-const ballImage = document.getElementById("ball")
+
+const answerdiv = document.getElementById("answer");
+const questiondiv = document.querySelector(".question");
+const questionText = document.querySelector(".questionText");
+const questionDisplay = document.querySelector(".questionDisplay");
+const askAgain = document.querySelector(".askAgain");
+
+
 askbutton.addEventListener("click", askFn)
+askAgain.addEventListener("click", askAgainFN)
 
 function askFn() {
-    const index = Math.floor(Math.random() * answers.length);
-    console.log(answers[index]);
-    document.querySelector('#askInput').value = answers[index]
-    ballImage.setAttribute("src", asklaterImg);
+    const randomNum = Math.floor(Math.random() * 20)+1;  // 
+    const imgsrc = "../magic/magic8ball_" + randomNum + ".png";
+    console.log(imgsrc)
+
+    const imgtag = document.createElement("img")
+
+
+    imgtag.setAttribute("src", imgsrc);
+    // answerdiv.innerHTML= ""
+    answerdiv.appendChild(imgtag);
+  questiondiv.classList.toggle("hidden");
+  questionDisplay.innerHTML= askInput.value;
+  questionText.classList.toggle("hidden");
+}
+
+function askAgainFN(){
+    askInput.value = ""
+    answerdiv.innerHTML= ""
+    questiondiv.classList.toggle("hidden");
+    questionText.classList.toggle("hidden");
+
 }
 
